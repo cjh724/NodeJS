96a15eaf63a91426a06abb2b280786196e54228a
cjh724 <jh.cho133104@gmail.com>
2018-09-16 13:42:10 +0900

    remove comments

--- app_file.js
+++ app_file.js
@@ -6,8 +6,8 @@ app.use(bodyParser.urlencoded({extended : false}));
 app.locals.pretty = true;       // jade 줄바꿈
 app.set('view engine', 'jade');
 app.set('views', './views_file');
+
 app.get('/topic/new', function(req, res) {
-    // res.send('Hi');
     fs.readdir('data', function(err, files) {
         if(err) {
             console.log(err);
@@ -16,6 +16,7 @@ app.get('/topic/new', function(req, res) {
         res.render('new', {topics : files});
     });
 });
+
 app.get(['/topic', '/topic/:id'], function(req, res) {
     fs.readdir('data', function(err, files) {
         if(err) {
@@ -38,33 +39,7 @@ app.get(['/topic', '/topic/:id'], function(req, res) {
         }
     });
 });
-/*
-app.get('/topic', function(req, res) {
-    fs.readdir('data', function(err, files) {
-        if(err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-        res.render('view', {topics : files});
-    });
-});
-app.get('/topic/:id', function(req, res) {
-    var id = req.params.id;
-    fs.readdir('data', function(err, files) {
-        if(err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-        fs.readFile('data/' + id, 'utf8', function(err, data) {
-            if(err) {
-                console.log(err);
-                res.status(500).send('Internal Server Error');
-            }
-            res.render('view', {topics : files, title : id, description : data});
-        });
-    });
-});
-*/
+
 app.post('/topic', function(req, res) {
     var title = req.body.title;
     var description = req.body.description;
